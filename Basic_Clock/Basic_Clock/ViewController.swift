5be2b2374fdd67ef0d2bfcc22aac133fc4e9271c
Sejin Lee <lsj8706@naver.com>
2022-06-30 00:58:17 +0900

    [FEAT] : 프로젝트 세팅

---
+++ Basic_Clock/Basic_Clock/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Basic_Clock
+//
+//  Created by sejin on 2022/06/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
