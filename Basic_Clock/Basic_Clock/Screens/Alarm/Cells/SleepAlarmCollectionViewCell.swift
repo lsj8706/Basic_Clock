ca78f917311b03f45100325cc79de59ffee9ab9e
Sejin Lee <lsj8706@naver.com>
2022-07-01 00:40:47 +0900

    [FEAT] : SleepAlarm Cell UI 구현

--- Basic_Clock/Basic_Clock/Screens/Alarm/Cells/SleepAlarmCollectionViewCell.swift
+++ Basic_Clock/Basic_Clock/Screens/Alarm/Cells/SleepAlarmCollectionViewCell.swift
@@ -10,9 +10,47 @@ import UIKit
 class SleepAlarmCollectionViewCell: UICollectionViewCell {
     static let identifier = "SleepAlarmCollectionViewCell"
     
+    private let alarmLabel = UILabel().then {
+        $0.text = "알람 없음"
+        $0.textColor = .lightGray
+        $0.font = UIFont.systemFont(ofSize: 18, weight: .regular)
+    }
+    
+    private let cellDivider = UIView().then {
+        $0.backgroundColor = .darkGray
+    }
+    
+    private let setupButton = UIButton().then {
+        let attributedString = NSAttributedString(string: "설정", attributes: [.font: UIFont.systemFont(ofSize: 16, weight: .heavy)])
+        $0.setAttributedTitle(attributedString, for: .normal)
+        $0.setTitleColor(.orange, for: .normal)
+        $0.backgroundColor = UIColor(named: "myDarkGray")
+        $0.layer.cornerRadius = 15
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        self.backgroundColor = .green
+        
+        addSubview(alarmLabel)
+        alarmLabel.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.equalToSuperview().offset(10)
+        }
+        
+        addSubview(cellDivider)
+        cellDivider.snp.makeConstraints { make in
+            make.top.equalTo(snp.bottom).inset(1)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(1)
+        }
+        
+        addSubview(setupButton)
+        setupButton.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.trailing.equalToSuperview().inset(10)
+            make.height.equalTo(30)
+            make.width.equalTo(45)
+        }
     }
     
     required init?(coder: NSCoder) {
