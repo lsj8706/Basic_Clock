25a8feecdb2f145e48e2ef09044ce8317ceb4360
Sejin Lee <lsj8706@naver.com>
2022-07-01 00:41:04 +0900

    [FEAT] : Alarm Cell UI 구현

--- Basic_Clock/Basic_Clock/Screens/Alarm/Cells/AlarmCollectionViewCell.swift
+++ Basic_Clock/Basic_Clock/Screens/Alarm/Cells/AlarmCollectionViewCell.swift
@@ -10,9 +10,50 @@ import UIKit
 class AlarmCollectionViewCell: UICollectionViewCell {
     static let identifier = "AlarmCollectionViewCell"
     
+    private let timeLabel = UILabel().then {
+        $0.text = "10:10"
+        $0.textColor = .lightGray
+        $0.font = UIFont.systemFont(ofSize: 50, weight: .regular)
+    }
+    
+    private let dayLabel = UILabel().then {
+        $0.text = "알람, 월 화 수"
+        $0.textColor = .lightGray
+        $0.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+    }
+    
+    private let cellDivider = UIView().then {
+        $0.backgroundColor = .darkGray
+    }
+    
+    private var alarmSwitch = UISwitch().then {
+        $0.setColor(offTint: UIColor(named: "myDarkGray")!)
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .blue
+
+        let stack = UIStackView(arrangedSubviews: [timeLabel, dayLabel])
+        stack.axis = .vertical
+        addSubview(stack)
+        stack.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.equalToSuperview().offset(10)
+        }
+        
+        addSubview(cellDivider)
+        cellDivider.snp.makeConstraints { make in
+            make.top.equalTo(snp.bottom).inset(1)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(1)
+        }
+        
+        addSubview(alarmSwitch)
+        alarmSwitch.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.trailing.equalToSuperview().inset(10)
+        }
+
     }
     
     required init?(coder: NSCoder) {
--- Basic_Clock/Basic_Clock/Utils/Extensions.swift
+++ Basic_Clock/Basic_Clock/Utils/Extensions.swift
@@ -5,4 +5,13 @@
 //  Created by sejin on 2022/07/01.
 //
 
-import Foundation
+import UIKit
+
+extension UISwitch {
+    func setColor(offTint color: UIColor ) {
+        let minSide = min(bounds.size.height, bounds.size.width)
+        layer.cornerRadius = minSide / 2
+        backgroundColor = color
+        tintColor = color
+    }
+}
